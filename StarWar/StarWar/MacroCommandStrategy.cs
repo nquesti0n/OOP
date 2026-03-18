0bedc8cab9e296012ab208fe9aaa7a48b303094d
nquesti0n <anstasika@gmail.com>
2026-03-18 20:33:30 +0600

    Add logging command for exception handling strategy

---
+++ StarWar/StarWar/MacroCommandStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Hwdtech;
+
+namespace StarWar;
+
+public interface IStrategy
+{
+    public object Init(params object[] args);
+}
+
+public class MacroCommandStrategy : IStrategy
+{
+    public object Init(params object[] args)
+    {
+        var nameOperation = (string)args[0];
+        var obj = (IUObject)args[1];
+
+        var dependencies = IoC.Resolve<string[]>("Component." + nameOperation);
+        var commands = dependencies.Select(dependency => IoC.Resolve<ICommand>(dependency, obj));
+
+        return IoC.Resolve<ICommand>("Game.Command.CreateMacro", commands);
+    }
+}
