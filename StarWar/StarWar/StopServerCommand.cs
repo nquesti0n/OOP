c8a2560915a574cfd73649546ca42bddf124171e
nquesti0n <anstasika@gmail.com>
2026-03-18 20:08:03 +0600

    Add StartServerCommand and StopServerCommand logic

---
+++ StarWar/StarWar/StopServerCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWar;
+using System.Collections.Concurrent;
+using Hwdtech;
+
+public class StopServerCommand : ICommand
+{
+    public void Execute()
+    {
+       
+        var threadQueues = IoC.Resolve<ConcurrentDictionary<int, BlockingCollection<ICommand>>>("Server.Thread.SenderDictionary");
+
+        threadQueues.ToList().ForEach(sender =>
+            IoC.Resolve<ICommand>(
+                "Server.Thread.SendCommand",
+                
+                sender.Key,
+               
+                IoC.Resolve<ICommand>("Server.Thread.SoftStop", sender.Key, IoC.Resolve<Action>("Server.Thread.SoftStop.Action"))
+            ).Execute()
+        );
+    }
+}
