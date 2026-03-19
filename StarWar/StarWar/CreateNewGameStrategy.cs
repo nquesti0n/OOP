bdb50efce92d8be20309ecb1d5b550acf422d503
nquesti0n <anstasika@gmail.com>
2026-03-19 18:49:16 +0600

    Стратегии

---
+++ StarWar/StarWar/CreateNewGameStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Hwdtech;
+
+namespace StarWar;
+
+public class CreateNewGameStrategy : IStrategy
+{
+    public object Init(params object[] args)
+    {
+        var gameID = (string)args[0];
+
+        var gameQueue = IoC.Resolve<Queue<ICommand>>("Game.Queue.CreateNew", gameID);
+        var gameScope = IoC.Resolve<IDictionary<string, object>>("Game.Scopes.Dictionary")[gameID];
+        var gameLikeCommand = IoC.Resolve<ICommand>("Game.Command", gameScope, gameQueue);
+
+        var bridgeCommand = IoC.Resolve<ICommand>("Game.Command.Bridge", gameLikeCommand);
+
+        var gamesDictionary = IoC.Resolve<IDictionary<string, ICommand>>("Game.Dictionary");
+        gamesDictionary.Add(gameID, bridgeCommand);
+
+        return bridgeCommand;
+    }
+}
